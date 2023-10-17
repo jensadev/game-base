19bf46abdb0ba41e2697c7cb61ac5530bba69b2a
Jens Andreasson <jensandreasson77@gmail.com>
2023-10-17 22:04:54 +0200

    ⚰️ removed unused grounded

--- src/Enemy.js
+++ src/Enemy.js
@@ -6,15 +6,10 @@ export default class Enemy {
     this.speedX = 0
     this.speedY = 0
     this.markedForDeletion = false
-    this.grounded = false
   }
 
   update() {
-    if (this.grounded) {
-      this.speedY = 0
-    } else {
-      this.speedY += this.game.gravity
-    }
+    this.speedY += this.game.gravity
 
     this.y += this.speedY
     this.x += this.speedX
